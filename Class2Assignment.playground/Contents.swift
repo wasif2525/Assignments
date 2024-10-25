8164293a2edb1e564b93ec8fbe948900e3826533
wasif2525 <Bhuiyan.Wasif@techconsulting.tech>
2024-10-25 00:37:42 -0400

    Tried the optional bindings

--- Class2Assignment.playground/Contents.swift
+++ Class2Assignment.playground/Contents.swift
@@ -30,6 +30,63 @@ for (index, element) in mixArray.enumerated() {
     print("Index: \(index), Element: \(element)")
 }
 
-mixArray.remove(at: 2)
-print(mixArray)
+
+var nums: Set = [1, 2, 3, 4, 4]
+print(nums)
+
+nums.insert(-3)
+nums.insert(5)
+print(nums)
+
+var stringSet: Set<String> = ["Mac", "John", "Chris", "c"]
+var anotherSet: Set = ["a", "c"]
+
+//common element between the two sets
+var commonElement = stringSet.intersection(anotherSet)
+print(commonElement)
+
+var all = stringSet.union(anotherSet)
+print(all)
+
+var withoutIntersection = all.subtracting(commonElement)
+print(withoutIntersection)
+
+for (index, element) in stringSet.enumerated() {
+    print("\(index):\(element)")
+}
+
+var newDict: [String: Int] = ["James":4, "John":5, "Chris":6]
+print(newDict)
+print(newDict["John"] ?? "Value Not found")
+
+if let value = newDict["John"] {
+    print(value)
+}
+
+newDict["James"] = 10
+newDict["Alex"] = 10
+newDict["Shaun"] = nil
+print(newDict)
+print(newDict["Shaun"] ?? "The value is nil for this key")
+
+var name = "James"
+@MainActor func useGuard() {
+    guard let value = newDict[name] else {
+        print("\(name) is not in the dictionary")
+        return
+    }
+    print(value)
+}
+useGuard()
+
+for (key, val) in newDict {
+    if val == 10 {
+        print("Found \(key), for whom the value is \(val)")
+    }
+}
+    
+
+print(newDict.keys)
+
+
 
