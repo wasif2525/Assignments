e662b4c3305dedbd6f9c6596041bd2d94bf6e466
wasif2525 <Bhuiyan.Wasif@techconsulting.tech>
2024-10-27 00:02:33 -0400

    Struct examples

--- Class2Assignment.playground/Pages/LoopsAndOptionals.xcplaygroundpage/Contents.swift
+++ Class2Assignment.playground/Pages/LoopsAndOptionals.xcplaygroundpage/Contents.swift
@@ -1,7 +1,93 @@
-//: [Previous](@previous)
+import UIKit
 
-import Foundation
+var scores: [Int] = [4, 7, 9, 3, 2]
+print(scores)
+print(scores.sorted())
 
-var greeting = "Hello, playground"
+let avgScore = 5
+let goodScore = 8
 
-//: [Next](@next)
+var i = 0
+while i < scores.count {
+    
+    if scores[i] >= goodScore {
+        print("Score \(scores[i]) is a good score")
+    }
+    else if scores[i] >= avgScore {
+        print("Score \(scores[i]) is an averge score")
+    }
+    else {
+        print("Score \(scores[i]) is not a good score")
+    }
+    i += 1
+}
+
+repeat{
+    print("Last score in the array: \(scores[i-1])")
+} while i < 0
+
+var mixArray: [Any] = [1, "Hello", true, 4.5]
+mixArray.append("Assignment 2")
+print(mixArray)
+
+for (index, element) in mixArray.enumerated() {
+    print("Index: \(index), Element: \(element)")
+}
+
+
+var nums: Set = [1, 2, 3, 4, 4]
+print(nums)
+
+nums.insert(-3)
+nums.insert(5)
+print(nums)
+
+var stringSet: Set<String> = ["Mac", "John", "Chris", "c"]
+var anotherSet: Set = ["a", "c"]
+
+//common element between the two sets
+var commonElement = stringSet.intersection(anotherSet)
+print(commonElement)
+
+var all = stringSet.union(anotherSet)
+print(all)
+
+var withoutIntersection = all.subtracting(commonElement)
+print(withoutIntersection)
+
+for (index, element) in stringSet.enumerated() {
+    print("\(index):\(element)")
+}
+
+var newDict: [String: Int] = ["James":4, "John":5, "Chris":6]
+print(newDict)
+print(newDict["John"] ?? "Value Not found")
+
+if let value = newDict["John"] {
+    print(value)
+}
+
+newDict["James"] = 10
+newDict["Alex"] = 10
+newDict["Shaun"] = nil
+print(newDict)
+print(newDict["Shaun"] ?? "The value is nil for this key")
+
+var name = "James"
+@MainActor func useGuard() {
+    guard let value = newDict[name] else {
+        print("\(name) is not in the dictionary")
+        return
+    }
+    print(value)
+}
+useGuard()
+
+for (key, val) in newDict {
+    if val == 10 {
+        print("Found \(key), for whom the value is \(val)")
+    }
+}
+    
+
+print(newDict.keys)
--- Class2Assignment.playground/Pages/Struct.xcplaygroundpage/Contents.swift
+++ Class2Assignment.playground/Pages/Struct.xcplaygroundpage/Contents.swift
@@ -1,7 +1,100 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+struct Book {
+    var title: String
+    var author: String
+    var pageCount: Int
+    var publisher: String?
+    var publisherDetails: PublisherDetails?
+    
+//    init(title: String, author: String, pageCount: Int) {
+//        self.title = title
+//        self.author = author
+//        self.pageCount = pageCount
+//    }
+    
+    //Structs automatically get a initializer if no custom one is provided.
+    //auto initializer will work if the props are not private
+    func description() -> String {
+        "\(title) by \(author). It contains \(pageCount) pages."
+    }
+    
+    //structs are immutable unlike classes
+    //to mutate need to add keyword 'mutating'
+    mutating func changePageCount(newPageCount: Int) {
+        pageCount = newPageCount
+        print("Updated page count to \(self.pageCount)")
+    }
+    
+    func getPublisherInfo() -> String {
+        
+        return publisher ?? "Publisher info is not available"
+//        if let pub = publisher {
+//            return "Published by \(pub)"
+//        } else {
+//            return "Publisher info is not available"
+//        }
+        
+//        guard let pub = publisher else {
+//            return "Publisher info is not available"
+//        }
+//        return "Published by \(pub)"
+    }
+}
 
-//: [Next](@next)
+
+var b1 = Book(title: "The Swift Programming Language", author: "Khalid Al-Balkhi", pageCount: 500)
+
+print(b1.description())
+
+var b2 = b1 //internally memory wise they each have their own separate copies of the data
+
+b2.pageCount = 200 //instance itself is mutable but struct isn't
+
+print("B2 page count changed to \(b2.pageCount)")
+print("B1 page count still remains as \(b1.pageCount)") //struct is value type and it gets stored on stack memory so it is much faster than reference type or classes
+//  whereas class is reference type and gets stored on heap memory
+
+b1.changePageCount(newPageCount: 499)
+
+print("---Optional Variable---")
+print(b1.getPublisherInfo())
+var b3 = Book(title: "The Swift Programming Language", author: "Khalid Al-Balkhi", pageCount: 500, publisher: "Ackermann Press")
+print(b3.getPublisherInfo())
+
+print("---Optional Chaining---")
+let characterCount = b3.publisher?.count ?? 0
+print("The number of character in Publisher is \(characterCount)")
+//Inheritance not possible for struct
+
+//struct StoryBook: Book {
+//    init(){
+//        super.init(title: String, author: String, pageCount: Int)
+//    }
+//    
+//}
+//var sb = StoryBook(title: "The Swift Programming Language", author: "Khalid Al-Balkhi", pageCount: 500)
+
+struct PublisherDetails {
+    var name: String
+    var address: String
+    
+    func getFullAddress() -> String {
+        return "\(name) \(address)"
+    }
+}
+
+let publisherInfo = PublisherDetails(name: "The Cranberry", address: "Atlanta, GA")
+var b5 = Book(title: "The Swift Programming Language", author: "Khalid Al-Balkhi", pageCount: 500, publisherDetails: publisherInfo)
+
+if let publisherName = b1.publisherDetails?.name{
+    print("Publisher name is \(publisherName)")
+} else {
+    print("Publisher name not available")
+}
+
+let pub2 = b5.publisherDetails?.address ?? "Publisher info not available"
+print("Publisher address is \(pub2)")
+
+
+
